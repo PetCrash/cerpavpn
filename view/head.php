a2b266797e2fde4975a455f152e581ca0209dba2
PetCrash <pedro.luis.cerpa@gmail.com>
2021-08-20 21:07:32 +0100

    Añadir views

---
+++ view/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="./resources/style.css" type="text/css" media="all" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+</head>
